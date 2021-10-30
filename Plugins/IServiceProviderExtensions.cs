be11be03396f9989b3c1c293988189bd001b54f7
melody-universe <tyler.hogsett@microsoft.com>
2021-10-30 10:16:51 -0700

    initial commit (currently supports Associate / Disassociate)

---
+++ Plugins/IServiceProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Mppce.PowerAutomateExtendedTriggers.Plugins
+{
+    static class IServiceProviderExtensions
+    {
+        public static IOrganizationService ResolveOrganizationServiceAsAdmin(
+            this IServiceProvider serviceProvider
+        ) => serviceProvider.ResolveOrganizationServiceFactory().CreateOrganizationService(null);
+
+        static IOrganizationServiceFactory ResolveOrganizationServiceFactory(
+            this IServiceProvider serviceProvider
+        ) => serviceProvider.Resolve<IOrganizationServiceFactory>();
+
+        public static IPluginExecutionContext ResolvePluginExecutionContext(
+            this IServiceProvider serviceProvider
+        ) => serviceProvider.Resolve<IPluginExecutionContext>();
+
+        static TOutput Resolve<TOutput>(this IServiceProvider serviceProvider) =>
+            (TOutput)serviceProvider.GetService(typeof(TOutput));
+    }
+}
---
+++ Plugins/OnAssociateDisassociate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Mppce.PowerAutomateExtendedTriggers.Plugins
+{
+    public class OnAssociateDisassociate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.ResolvePluginExecutionContext();
+
+            var message = context.MessageName;
+
+            var target = (EntityReference)context.InputParameters["Target"];
+            var relatedEntities = (EntityReferenceCollection)context.InputParameters[
+                "RelatedEntities"
+            ];
+            var relationship = (Relationship)context.InputParameters["Relationship"];
+
+            var service = serviceProvider.ResolveOrganizationServiceAsAdmin();
+
+            foreach (var relatedEntity in relatedEntities)
+            {
+                var request = new OrganizationRequest($"mppce_OnAssociateDisassociate");
+                request["Message"] = message;
+                request["Relationship"] = relationship.SchemaName;
+                request["Entity1"] = target;
+                request["Entity2"] = relatedEntity;
+
+                service.Execute(request);
+            }
+        }
+    }
+}
